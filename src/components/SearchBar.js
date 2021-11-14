fe3426d11fe41dc6faf3c61324747b2af34a6495
Isa <isa@aboutbirdie.com>
2021-11-14 23:46:33 +0100

    converting class components to functions, custom hook

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,25 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
 
-class SearchBar extends Component {
-    state = { term: '' }
-    onInputChange = event => {
-        this.setState({term: event.target.value})
-    };
-    onFormSubmit = event => {
-      event.preventDefault();
-      this.props.onSubmit(this.state.term);
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
 
-    };
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                <div className="field">
-                    <label>Video Search</label>
-                    <input
-                        type="text"
-                        onChange={this.onInputChange}
-                        value={this.state.term} />
-                </div>
-                </form>
-            </div>
-        );
-    }
-}
-
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit = term;
+  };
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onFormSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            onChange={(event) => setTerm(event.target.value)}
+            value={term}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 export default SearchBar;
